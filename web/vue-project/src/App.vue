50d33b81eb2dc241b81cea682eeedcbb6a5188fb
SaraMartinez21 <123445716+SaraMartinez21@users.noreply.github.com>
2024-04-14 19:53:06 +0200

    funcionaaa

--- web/vue-project/src/App.vue
+++ web/vue-project/src/App.vue
@@ -1,47 +1,21 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <table>
+      <tr>
+        <td @click="console.log(1)">1</td>
+        <td @click="console.log(2)">2</td>
+        <td @click="console.log(3)">3</td>
+      </tr>
+      <tr>
+        <td @click="console.log(4)">4</td>
+        <td @click="console.log(5)">5</td>
+        <td @click="console.log(6)">6</td>
+      </tr>
+      <tr>
+        <td @click="console.log(7)">7</td>
+        <td @click="console.log(8)">8</td>
+        <td @click="console.log(9)">9</td>
+      </tr>
+    </table>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
